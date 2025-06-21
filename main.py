ce91118c376267804f9c60d7aaef07d4bb763c80
skatzrsk <skatzr1997@gmail.com>
2025-06-21 15:35:54 +0700

    Change port to 10000

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ async def process_query(query: str):
 # Точка входа для запуска сервера
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
